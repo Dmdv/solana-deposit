6d5ef564e476a638e7d5016b95eeba48fe53c43e
Dmitry Dyachkov <dima.dyachkov@gmail.com>
2025-05-06 17:56:18 +0500

    Update keys and deployment

--- client/src/bin/check_balance.rs
+++ client/src/bin/check_balance.rs
@@ -5,10 +5,11 @@ use {
 };
 
 fn main() -> Result<()> {
+    // Connect to localhost validator
     let rpc_client = RpcClient::new("http://localhost:8899");
 
     // Deposit account pubkey from the previous run
-    let deposit_account_pubkey = "Ctqpa8nH5cCbFmUh2f1HgqgPStSAVehwNMWzjKj4WFkz";
+    let deposit_account_pubkey = "CfybBwktjRTQ3ZicJVk9Y7gF9rynY3egtucEc1Zy66ws";
 
     // Get and print the balance
     let balance = get_balance(&rpc_client, deposit_account_pubkey)?;
